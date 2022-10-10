aa27c6430171d77f6d7f544e56544d84cf47c19b
JohanSannier <johan.sanniercaffier@gmail.com>
2022-10-10 10:48:18 +0200

    header left part

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import GlobalChart from "./components/GlobalChart";
+import HeaderInfos from "./components/HeaderInfos";
 import "./styles/index.scss";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div className="app-container">
+        <header>
+          <HeaderInfos />
+          <GlobalChart />
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/GlobalChart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GlobalChart = () => {
+  return (
+    <div className="global-chart">
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default GlobalChart;
